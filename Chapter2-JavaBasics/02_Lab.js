358260c6e29ddd915354730edfd99e68f06c1ec0
sripraveenkvs-bit <sripraveen.kvs@gmail.com>
2026-02-17 08:13:55 +0530

    Update lab and test files

---
+++ Chapter2-JavaBasics/02_Lab.js
@@ -0,0 +1,24 @@
+
+// let b ="srirama";
+// console.log(b);
+
+// console.log(greetings);
+// var greetings = "Hello, World!";
+// console.log(greetings);
+
+function test() {
+console.log(greetings);
+var greetings = "Hello, World!";
+console.log(greetings);
+}
+test();
+
+//LET Hosting
+let greetings = "Srirama";
+function testLet() {
+   // console.log(greetings);
+    let greetings = "Hello, World!";
+    console.log(greetings);
+}
+testLet();
+console.log(greetings);
